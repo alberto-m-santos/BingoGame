0ddc8149602e53d91e1fa4219bc44893ec342e4f
alberto-m-santos <aslms.tic@gmail.com>
2021-09-03 15:37:55 +0100

    class player

--- main.cpp
+++ main.cpp
@@ -1,17 +1,39 @@
-#include <bits/stdc++.h>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <cctype>
+#include<string>
+#include<windows.h>
 using namespace std;
+
+
+class Player
+{
+private:
+    bool winner = false;
+
+public:
+    int card[3][5]={{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+    void Readcard();
+    void Marknumber (int x);
+    int credits;
+};
+
+void Player::Readcard()
+{
+    for(int row=0; row<3; row++){
+        for(int col=0; col<5; col++){
+            cout << card[row][col]<<" ";
+        }
+        cout << endl;
+    }
+}
+
+
 
 int main()
 {
-    int numero;
-    cout << "numero" << endl;
-    cout << "numero" << endl;
-    cout << "numero" << endl;
-    cout << "numero" << endl;
-    cin >> numero;
-
-    return numero;
+    return 0;
 }
